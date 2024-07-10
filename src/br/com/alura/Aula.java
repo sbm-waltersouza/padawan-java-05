c24d0d245e0c66fb6bed63e66bdb82e67c7c2c88
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-10 15:01:59 -0300

    Listas de objetos

---
+++ src/br/com/alura/Aula.java
@@ -0,0 +1,35 @@
+package br.com.alura;
+
+public class Aula implements Comparable<Aula>{
+
+	private String titulo;
+	private int tempo;
+
+	public Aula(String titulo, int tempo) {
+		super();
+		this.titulo = titulo;
+		this.tempo = tempo;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getTempo() {
+		return tempo;
+	}
+
+	@Override
+	public String toString() {
+		return "[Aulas: " + this.titulo + ", " + this.tempo + " minutos]";
+	}
+
+	@Override
+	public int compareTo(Aula outraAula) {
+		if(this.titulo.compareTo(outraAula.titulo) < 0) {
+			return this.titulo.compareTo(outraAula.titulo);
+		}
+		
+		return 0;
+	}
+}
