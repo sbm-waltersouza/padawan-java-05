f3d6a05fab8cd6dbcdac77daa0b80f4257c3f5fa
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-11 15:49:45 -0300

    Mapas

--- src/br/com/alura/QualColecaoUsar.java
+++ src/br/com/alura/QualColecaoUsar.java
@@ -7,6 +7,6 @@ public class QualColecaoUsar {
 
     public static void main(String[] args) {
 
-        Collection<Aluno> alunos = new ArrayList<>();    
+        //Collection<Aluno> alunos = new ArrayList<>();    
     }
 }
